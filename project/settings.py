b3fc9c4245f0b2207ad6f9d7701055c014fe37d7
nayandattabarua <nayandattabarua122221@gmail.com>
2024-02-10 19:43:26 +0600

    static

--- project/settings.py
+++ project/settings.py
@@ -154,13 +154,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "news/static")
+    os.path.join(BASE_DIR, "news/static/")
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage",
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage",
 
 # STORAGES = {
-#     # ...
 #     "static": {
 #         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
 #     },
